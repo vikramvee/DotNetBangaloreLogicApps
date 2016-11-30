dd1991c662856b66b350e2b74adac2fd1cff3086
vikramvee <vikram.vee@outlook.com>
2016-11-30 06:56:42 +0530

    Initial Check in
    Initial Check in

---
+++ QueueSender/MyWebApi/Controllers/hOMEController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace MyWebApi.Controllers
+{
+    public class hOMEController : ApiController
+    {
+        public string Get()
+        {
+            return "Hello Vikram";
+        }
+
+        public HttpResponseMessage Post(Person per)
+        {
+            return Request.CreateResponse(HttpStatusCode.OK, new { FullName = string.Format("{0} {1}", per.FirstName, per.LastName) });
+        }
+    }
+
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Gender { get; set; }
+        public int Age { get; set; }
+    }
+}
---
+++ QueueSender/QueueSender/Program.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.ServiceBus.Messaging;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QueueSender
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SendToQueue();
+        }
+
+        private static void SendToQueue()
+        {
+            Person per1 = new Person() { FirstName = "Vikram", LastName = "Chaudhary", Age = 33, Gender = "Male" };
+            var qc = QueueClient.Create("logicappmessage", ReceiveMode.PeekLock);
+            string personJson = JsonConvert.SerializeObject(per1);
+            qc.Send(new BrokeredMessage(GenerateStreamFromString(personJson))
+            {
+                TimeToLive = TimeSpan.FromMinutes(30),
+                Properties = { { "PersonData", "PersonData" } }
+
+            });
+        }
+
+        public static Stream GenerateStreamFromString(string s)
+        {
+            MemoryStream stream = new MemoryStream();
+            StreamWriter writer = new StreamWriter(stream);
+            writer.Write(s);
+            writer.Flush();
+            stream.Position = 0;
+            return stream;
+        }
+    }
+
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Gender { get; set; }
+        public int Age { get; set; }
+    }
+}
